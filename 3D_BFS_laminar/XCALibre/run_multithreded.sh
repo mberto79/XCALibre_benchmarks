a9dcd5fa353c4f562f9f5150885fdbc3cdbe621d
Humberto <h.medina@aerofluids.org>
2024-11-21 11:46:11 +0000

    add script files to set up folder structure

---
+++ 3D_BFS_laminar/XCALibre/run_multithreded.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# fileName="0.3.2"
+
+# julia -e 'using Pkg; Pkg.rm("XCALibre"); Pkg.add(name="XCALibre", version="0.3.2")'
+
+# julia -t 1 BFS_cpu.jl $fileName
+# julia -t 2 BFS_cpu.jl $fileName
+# julia -t 4 BFS_cpu.jl $fileName
+# julia -t 6 BFS_cpu.jl $fileName
+# julia -t 8 BFS_cpu.jl $fileName
+
+
+fileName="0.3.3"
+
+julia -e 'using Pkg; Pkg.rm("XCALibre"); Pkg.develop(path="/home/humberto/JULIA/XCALibre.jl/")'
+
+julia -t 1 BFS_cpu.jl $fileName
+julia -t 2 BFS_cpu.jl $fileName
+julia -t 4 BFS_cpu.jl $fileName
+julia -t 6 BFS_cpu.jl $fileName
+julia -t 8 BFS_cpu.jl $fileName
+
+rm -rf *.vtk
+rm -rf *.vtu
