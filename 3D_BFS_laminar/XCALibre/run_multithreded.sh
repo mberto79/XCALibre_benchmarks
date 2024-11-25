2f19525c02776fd1bd14f8c65c4d9c3e49e9f0ab
Humberto <h.medina@aerofluids.org>
2024-11-25 23:50:41 +0000

    rerun benchmarks with Julia 1.11.1 and latest dev version of XCALibre

--- 3D_BFS_laminar/XCALibre/run_multithreded.sh
+++ 3D_BFS_laminar/XCALibre/run_multithreded.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-fileName="0.3.2"
+# fileName="0.3.2"
 
-julia -e 'using Pkg; Pkg.rm("XCALibre"); Pkg.add(name="XCALibre", version="0.3.2")'
+# julia -e 'using Pkg; Pkg.rm("XCALibre"); Pkg.add(name="XCALibre", version="0.3.2")'
 
-julia -t 1 BFS_cpu.jl $fileName
-julia -t 2 BFS_cpu.jl $fileName
-julia -t 4 BFS_cpu.jl $fileName
-julia -t 6 BFS_cpu.jl $fileName
-julia -t 8 BFS_cpu.jl $fileName
+# julia -t 1 BFS_cpu.jl $fileName
+# julia -t 2 BFS_cpu.jl $fileName
+# julia -t 4 BFS_cpu.jl $fileName
+# julia -t 6 BFS_cpu.jl $fileName
+# julia -t 8 BFS_cpu.jl $fileName
 
 
 fileName="0.3.3"
 
-julia -e 'using Pkg; Pkg.rm("XCALibre"); Pkg.develop(path="/home/humberto/JULIA/XCALibre.jl/")'
+julia -e 'using Pkg; "XCALibre" in keys(Pkg.project().dependencies) && Pkg.rm("XCALibre"); Pkg.develop(path="/home/humberto/JULIA/XCALibre.jl/")'
 
 julia -t 1 BFS_cpu.jl $fileName
 julia -t 2 BFS_cpu.jl $fileName
