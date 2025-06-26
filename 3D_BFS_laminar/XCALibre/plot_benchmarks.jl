e6acad0136ac31303da5d2f7e5c777dfe025e11e
Humberto <h.medina@aerofluids.org>
2025-06-26 04:06:32 +0100

    update plots

--- 3D_BFS_laminar/XCALibre/plot_benchmarks.jl
+++ 3D_BFS_laminar/XCALibre/plot_benchmarks.jl
@@ -7,16 +7,19 @@ isdir("figures") || mkdir("figures")
 # Load benchmark results
 of_similar = readdlm("openfoam_PCG_PBiCGStab_diag.txt", ',')
 of_GAMG = readdlm("openfoam_GAMG_PBiCGStab_DILU.txt", ',')
-xcal_033 = readdlm("multithread_0.3.3.txt", ',')
 xcal_032 = readdlm("multithread_0.3.2.txt", ',')
-xcal_033_gpu = readdlm("nvidia_0.3.3.txt", ',')
+xcal_033 = readdlm("multithread_0.3.3.txt", ',')
+xcal_050 = readdlm("multithread_0.5.0.txt", ',')
 xcal_032_gpu = readdlm("nvidia_0.3.2.txt", ',')
+xcal_033_gpu = readdlm("nvidia_0.3.3.txt", ',')
+xcal_050_gpu = readdlm("nvidia_0.5.0.txt", ',')
 
 # Plot execution time
 default()
 default(
     linewidth=2, marker=:circle, markersize=3, framestyle=:box, 
-    foreground_color_legend = nothing, background_color_legend=nothing
+    foreground_color_legend = nothing, background_color_legend=nothing,
+    yaxis = :log10
     )
 plot(
     xlabel="# processors", ylabel="Execution time (s)", 
@@ -24,13 +27,16 @@ plot(
     )
 plot!(of_similar[:,1], of_similar[:,2], label="OF11 Similar")
 plot!(of_GAMG[:,1], of_GAMG[:,2], label="OF11 Best")
-plot!(xcal_033[:,1], xcal_033[:,3], label="v0.3.3")
 plot!(xcal_032[:,1], xcal_032[:,3], label="v0.3.2")
-gpu_time_032 = xcal_033_gpu[1,2]
-gpu_time_031 = xcal_032_gpu[1,2]
+plot!(xcal_033[:,1], xcal_033[:,3], label="v0.3.3")
+plot!(xcal_050[:,1], xcal_050[:,3], label="v0.3.3")
+gpu_time_032 = xcal_032_gpu[1,2]
+gpu_time_033 = xcal_033_gpu[1,2]
+gpu_time_050 = xcal_050_gpu[1,2]
 
-plot!([1,8], [gpu_time_032, gpu_time_032], color=3,marker=:none, label="v0.3.3 GPU")
-plot!([1,8], [gpu_time_031, gpu_time_031], color=4, marker=:none, label="v0.3.2 GPU")
+plot!([1,8], [gpu_time_032, gpu_time_032], color=4, marker=:none, label="v0.3.2 GPU")
+plot!([1,8], [gpu_time_033, gpu_time_033], color=3,marker=:none, label="v0.3.3 GPU")
+plot!([1,8], [gpu_time_050, gpu_time_050], color=3,marker=:none, label="v0.3.3 GPU")
 savefig("figures/execution_time_comparision.svg")
 savefig("figures/execution_time_comparision.png")
 
@@ -42,13 +48,16 @@ plot(
     )
 plot!(of_similar[:,1], ref./of_similar[:,2], label="OF11 Similar")
 plot!(of_GAMG[:,1], ref./of_GAMG[:,2], label="OF11 Best")
-plot!(xcal_033[:,1], ref./xcal_033[:,3], label="v0.3.3")
 plot!(xcal_032[:,1], ref./xcal_032[:,3], label="v0.3.2")
-gpu_time_032 = ref./xcal_033_gpu[1,2]
-gpu_time_031 = ref./xcal_032_gpu[1,2]
+plot!(xcal_033[:,1], ref./xcal_033[:,3], label="v0.3.3")
+plot!(xcal_050[:,1], ref./xcal_050[:,3], label="v0.3.3")
+gpu_time_032 = ref./xcal_032_gpu[1,2]
+gpu_time_033 = ref./xcal_033_gpu[1,2]
+gpu_time_050 = ref./xcal_050_gpu[1,2]
 
-plot!([1,8], [gpu_time_032, gpu_time_032], color=3,marker=:none, label="v0.3.3 GPU")
-plot!([1,8], [gpu_time_031, gpu_time_031], color=4, marker=:none, label="v0.3.2 GPU")
+plot!([1,8], [gpu_time_032, gpu_time_032], color=4, marker=:none, label="v0.3.2 GPU")
+plot!([1,8], [gpu_time_033, gpu_time_033], color=3,marker=:none, label="v0.3.3 GPU")
+plot!([1,8], [gpu_time_050, gpu_time_050], color=3,marker=:none, label="v0.3.3 GPU")
 savefig("figures/speedup_vs_v0.3.2.svg")
 savefig("figures/speedup_vs_v0.3.2.png")
 
@@ -59,8 +68,9 @@ plot(
     )
 plot!(of_similar[:,1], of_similar[1,2]./of_similar[:,2], label="OF11 Similar")
 plot!(of_GAMG[:,1], of_GAMG[1,2]./of_GAMG[:,2], label="OF11 Best")
-plot!(xcal_033[:,1], xcal_033[1,3]./xcal_033[:,3], label="v0.3.3")
 plot!(xcal_032[:,1], xcal_032[1,3]./xcal_032[:,3], label="v0.3.2")
+plot!(xcal_033[:,1], xcal_033[1,3]./xcal_033[:,3], label="v0.3.3")
+plot!(xcal_050[:,1], xcal_050[1,3]./xcal_050[:,3], label="v0.3.3")
 plot!(1:8, 1:8, label="Ideal scaling", color=:black)
 
 savefig("figures/parallel_scaling.svg")
