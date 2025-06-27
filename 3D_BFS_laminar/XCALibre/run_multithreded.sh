c9e8a808d14d513b3b120dc391e9e9e81282e01e
Humberto <h.medina@aerofluids.org>
2025-06-28 00:06:50 +0100

    remove low thread tests

--- 3D_BFS_laminar/XCALibre/run_multithreded.sh
+++ 3D_BFS_laminar/XCALibre/run_multithreded.sh
@@ -28,10 +28,10 @@ fileName="0.5.0"
 julia -t 8 BFS_env_clean.jl
 julia -t 8 BFS_env_setup_v0.5.0.jl $fileName
 
-julia -t 1 BFS_cpu_v0.5.0.jl $fileName
-julia -t 2 BFS_cpu_v0.5.0.jl $fileName
+#julia -t 1 BFS_cpu_v0.5.0.jl $fileName
+#julia -t 2 BFS_cpu_v0.5.0.jl $fileName
 julia -t 4 BFS_cpu_v0.5.0.jl $fileName
-julia -t 6 BFS_cpu_v0.5.0.jl $fileName
+#julia -t 6 BFS_cpu_v0.5.0.jl $fileName
 julia -t 8 BFS_cpu_v0.5.0.jl $fileName
 
 rm -rf *.vtk
